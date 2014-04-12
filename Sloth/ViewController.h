85729eca8489e7de489d43023dc3098013d9c0df
Samir Lavingia <lavingiasa@gmail.com>
2014-04-12 02:25:10 -0700

    It now prints the x and y location

--- Sloth/ViewController.h
+++ Sloth/ViewController.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+{
+  CLLocationManager *locationManager;
+
+}
+
+
+@property (strong, nonatomic) IBOutlet UIButton *checkIn;
 
 @end
--- Sloth/ViewController.m
+++ Sloth/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +28,39 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)checkIn:(id)sender
+{
+  NSLog(@"You are checking in now!");
+  locationManager.delegate = self;
+  locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  
+  [locationManager startUpdatingLocation];
+
+  //get current location
+  //check if you are in the right place
+  //if you are in the right place then nothing, if you are not then print error
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    NSLog(@"OldLocation %f %f", oldLocation.coordinate.latitude, oldLocation.coordinate.longitude);
+    NSLog(@"NewLocation %f %f", newLocation.coordinate.latitude, newLocation.coordinate.longitude);
+    [locationManager stopUpdatingLocation];
+
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+  NSLog(@"didFailWithError: %@", error);
+  UIAlertView *errorAlert = [[UIAlertView alloc]
+                             initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+  [errorAlert show];
+}
+
+- (IBAction)cancelClass:(id)sender
+{
+  NSLog(@"You are canceling now!");
 }
 
 @end

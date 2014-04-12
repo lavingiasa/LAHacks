c6088a30c9fa58184383c1d3d84a1c70af3f2ecf
Samir Lavingia <lavingiasa@gmail.com>
2014-04-12 05:09:28 -0700

    Made it so the calendar table view controller works and shit!

---
+++ Sloth/SectionsTable.h
@@ -0,0 +1,13 @@
+//
+//  SectionsTable.h
+//  Sloth
+//
+//  Created by Samir Lavingia on 12/04/2014.
+//  Copyright (c) 2014 SKAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SectionsTable : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
